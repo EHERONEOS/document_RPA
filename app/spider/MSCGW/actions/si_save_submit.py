21ea839e4b6534ff411f157bf3fc3df263034c1d
wangchao <ysyxneos@163.com>
2026-09-20 09:18:40 +0800

    12212

--- app/spider/MSCGW/actions/si_save_submit.py
+++ app/spider/MSCGW/actions/si_save_submit.py
@@ -26,6 +26,44 @@ class SiSaveSubmitMixin:
     def save_submit(self: "FhtMscgwSiTask") -> None:
         if self.splitOrConsolidatedBill:
             raise BusinessError("并单拆单还未支持保存提交 请通知RPA处理")
+            self.si_shadow.click(selectors.FREE_SAVE_BOOKING_BTN, name="保存按钮")
+            is_save_success = self.si_shadow._find(
+                selectors.SAVE_SUCCESS_MSG, name="保存成功消息", timeout=10, required=False
+            )
+            if not is_save_success:
+                raise BusinessError("拆并单保存失败")
+            self.si_shadow.click(selectors.SAVE_SUCCESS_OK, name="保存成功确认按钮")
+            self.page.wait.doc_loaded()
+            self.si_shadow = self.dom.get_shadow_root(selectors.FREE_ESI_SHADOW, timeout=20)
+            for _ in range(30):
+                preview_btn = self.si_shadow._find(selectors.PREVIEW_BTN, name="预览按钮", timeout=1, required=False)
+                if preview_btn and preview_btn.states.is_enabled:
+                    preview_btn.click()
+                    break
+                time.sleep(1)
+            else:
+                raise BusinessError("页面刷新后预览按钮未可用")
+            for _ in range(30):
+                download_preview_btn = self.si_shadow._find(
+                    selectors.DOWNLOAD_PREVIEW_BTN, name="下载预览按钮", timeout=1, required=False
+                )
+                if download_preview_btn and download_preview_btn.states.is_enabled:
+                    file_path = self.dom.click_to_download(
+                        download_preview_btn,
+                        rename=self._build_draft_file_stem(),
+                        name="下载预览按钮",
+                    )
+                    self.attachments.append(file_path)
+                    break
+                time.sleep(1)
+            self.si_shadow.click(selectors.DOWNLOAD_CLOSE_BTN, name="关闭下载预览按钮")
+            if self.context.rpa_operate == "SUBMIT_DIRECT":
+                self.si_shadow.click(selectors.SUBMIT_BTN, name="提交按钮")
+                is_submit_success = self.si_shadow._find(
+                    selectors.SUBMIT_SUCCESS_MSG, name="提交成功消息", timeout=30, required=False
+                )
+                if not is_submit_success:
+                    raise BusinessError("截单提交失败")
 
         self.si_shadow.click(selectors.SAVE_BOOKING_BTN, name="保存按钮")
         is_save_success = self.si_shadow._find(
